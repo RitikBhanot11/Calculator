36d41682d18f223ec5916e0cae02624ae01d5ff0
Ritik Bhanot <bhanotritik11@gmail.com>
2026-05-17 01:17:28 +0300

    First Project

---
+++ calculate.py
@@ -0,0 +1,7 @@
+a=int(input("Write first number: "))
+b=int(input("Write second number: "))
+
+print("Addition:  ", a+b)
+print("Subtraction:  ", a-b)
+print("Multiplication:  ", a*b)
+print("Division:  ", a/b)
