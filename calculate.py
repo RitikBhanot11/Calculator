b0ade8ff186356860e7b0bad3435f83327a94cbd
Ritik Bhanot <bhanotritik11@gmail.com>
2026-05-17 02:24:45 +0300

    Updated

--- calculate.py
+++ calculate.py
@@ -1,7 +1,15 @@
 a=int(input("Write first number: "))
 b=int(input("Write second number: "))
+(print("Select the operation you want to perform: \n + for Addition \n - for Subtraction\n * for Multiplication\n / for Division"))
+x = input("Type here: ")
 
-print("Addition:  ", a+b)
-print("Subtraction:  ", a-b)
-print("Multiplication:  ", a*b)
-print("Division:  ", a/b)
+if x=="+":
+    print("Addition:  ", a+b)
+elif x=="-":
+    print("Subtraction:  ", a-b)
+elif x=="*":
+    print("Multiplication:  ", a*b)
+elif x=="/":
+    print("Division:  ", a/b)
+else:
+    print("Wrong Input!")
